4339950340848bd14d53f50be390fb4e5c7af3fa
jhekasoft <jhekasoft@gmail.com>
2019-01-22 20:35:58 +0200

    Change data structure

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sort"
 	"time"
 
 	"github.com/ghodss/yaml"
@@ -20,21 +21,28 @@ func main() {
 }
 
 func GetCalendar(w http.ResponseWriter, r *http.Request) {
+	monthTitles, err := parseMonthes("data/calendar/monthes.yml")
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+	// fmt.Printf("data: %v\n", monthTitles)
+
 	// Read ESV calendar
 	esv, err := parseCalendarData("data/calendar/2019/fop_single/esv.yml")
 	if err != nil {
 		fmt.Printf("err: %v\n", err)
 		return
 	}
-	fmt.Printf("data: %v\n", esv)
+	// fmt.Printf("data: %v\n", esv)
 
 	// Read Group 1, 2 calendar
-	taxGroup12, err := parseCalendarData("data/calendar/2019/fop_single/tax_group1_2.yml")
-	if err != nil {
-		fmt.Printf("err: %v\n", err)
-		return
-	}
-	fmt.Printf("data: %v\n", taxGroup12)
+	// taxGroup12, err := parseCalendarData("data/calendar/2019/fop_single/tax_group1_2.yml")
+	// if err != nil {
+	// 	fmt.Printf("err: %v\n", err)
+	// 	return
+	// }
+	// fmt.Printf("data: %v\n", taxGroup12)
 
 	// Read Group 3 calendar
 	taxGroup3, err := parseCalendarData("data/calendar/2019/fop_single/tax_group3.yml")
@@ -42,90 +50,87 @@ func GetCalendar(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("err: %v\n", err)
 		return
 	}
-	fmt.Printf("data: %v\n", taxGroup3)
+	// fmt.Printf("data: %v\n", taxGroup3)
 
-	data, err := combineCalendarData(append(esv, taxGroup3...), 3, false)
+	data, err := combineCalendarData(monthTitles, append(esv, taxGroup3...), 3, false)
 	if err != nil {
 		fmt.Printf("err: %v\n", err)
 		return
 	}
 
-	// t, err := time.Parse(layout, str)
-
-	// var monthes []Month
-	// monthes = append(monthes, Month{
-	// 	Number: 1,
-	// 	Title:  "Січень",
-	// 	Items:  []MonthItem{MonthItem{EndDate: "01.01.2019", Title: "Test", Amount: "100"}},
-	// })
-
 	json.NewEncoder(w).Encode(data)
 }
 
-func combineCalendarData(data []InputFopSingleMonth, group int, isWithPdv bool) (result []Month, err error) {
-	monthTitles := map[int]string{
-		1:  "Січень",
-		2:  "Лютий",
-		3:  "Березень",
-		4:  "Квітень",
-		5:  "Травень",
-		6:  "Червень",
-		7:  "Липень",
-		8:  "Серпень",
-		9:  "Вересень",
-		10: "Жовтень",
-		11: "Листопад",
-		12: "Грудень",
+func combineCalendarData(monthTitles map[int]string, data []InputFopCalendarSingleItem, group int, isWithPdv bool) (result []Month, err error) {
+	allMonthItems := []MonthItem{}
+
+	for _, inputCalendarItem := range data {
+		endData, err := time.Parse("02.01.2006", inputCalendarItem.EndDate)
+		if err != nil {
+			return nil, err
+		}
+
+		var amountMax float32
+		if group == 1 {
+			amountMax = inputCalendarItem.AmountMaxGroup1
+		} else if group == 2 {
+			amountMax = inputCalendarItem.AmountMaxGroup2
+		}
+
+		var amountPercents int
+		if group == 3 && isWithPdv {
+			amountPercents = inputCalendarItem.AmountPercentsPdv
+		} else if group == 3 && !isWithPdv {
+			amountPercents = inputCalendarItem.AmountPercentsNoPdv
+		}
+
+		allMonthItems = append(allMonthItems, MonthItem{
+			EndDate:        endData,
+			Title:          inputCalendarItem.Title,
+			Amount:         inputCalendarItem.Amount,
+			AmountMax:      amountMax,
+			AmountPercents: amountPercents,
+		})
 	}
 
-	for i := 1; i <= 12; i++ {
+	for monthNum, monthTitle := range monthTitles {
 		monthItems := []MonthItem{}
 
-		for _, inputMonth := range data {
-			if inputMonth.Num == i {
-				for _, inputMonthItem := range inputMonth.Items {
-					endData, err := time.Parse("02.01.2006", inputMonthItem.EndDate)
-					if err != nil {
-						return nil, err
-					}
-
-					var amountMax float32
-					if group == 1 {
-						amountMax = inputMonthItem.AmountMaxGroup1
-					} else if group == 2 {
-						amountMax = inputMonthItem.AmountMaxGroup2
-					}
-
-					var amountPercents int
-					if group == 3 && isWithPdv {
-						amountPercents = inputMonthItem.AmountPercentsPdv
-					} else if group == 3 && !isWithPdv {
-						amountPercents = inputMonthItem.AmountPercentsNoPdv
-					}
-
-					monthItems = append(monthItems, MonthItem{
-						EndDate:        endData,
-						Title:          inputMonthItem.Title,
-						Amount:         inputMonthItem.Amount,
-						AmountMax:      amountMax,
-						AmountPercents: amountPercents,
-					})
-				}
+		for _, item := range allMonthItems {
+			if int(item.EndDate.Month()) == monthNum {
+				monthItems = append(monthItems, item)
 			}
 		}
 
 		month := Month{
-			Number: i,
-			Title:  monthTitles[i],
+			Number: monthNum,
+			Title:  monthTitle,
 			Items:  monthItems,
 		}
 		result = append(result, month)
 	}
 
+	sort.Slice(result, func(i, j int) bool {
+		return result[i].Number < result[j].Number
+	})
+
 	return
 }
 
-func parseCalendarData(path string) (parsedData []InputFopSingleMonth, err error) {
+func parseCalendarData(path string) (parsedData []InputFopCalendarSingleItem, err error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return
+	}
+	err = yaml.Unmarshal(data, &parsedData)
+	if err != nil {
+		return
+	}
+
+	return parsedData, nil
+}
+
+func parseMonthes(path string) (parsedData map[int]string, err error) {
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		return
@@ -140,11 +145,7 @@ func parseCalendarData(path string) (parsedData []InputFopSingleMonth, err error
 
 // Input data
 // TODO: move to the other file
-type InputFopSingleMonth struct {
-	Num   int                       `json:"num"`
-	Items []InputFopSingleMonthItem `json:"items"`
-}
-type InputFopSingleMonthItem struct {
+type InputFopCalendarSingleItem struct {
 	EndDate             string  `json:"end_date"`
 	Type                string  `json:"type"`
 	Title               string  `json:"title"`
